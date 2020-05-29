3a91fe03eaea0514f2098f7943551cf244aee875
Benw8888 <39271586+Benw8888@users.noreply.github.com>
2020-05-28 22:57:56 -0400

    Update Main.java

--- chess/Main.java
+++ chess/Main.java
@@ -9,6 +9,10 @@ public class Main {
     /** Opens and begins a new game of chess. */
     public static void main(String... dummy) {
         Game game = new Game();
+        List<Move> validMoves=game.listValidMoves();
+        for(Move m : validMoves) {
+        	System.out.println(m.movedPiece().type().abbrev());
+        }
     }
 
 }
