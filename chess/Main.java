db151891925b78d8f70511dd0c12ffe40477360a
Benw8888 <39271586+Benw8888@users.noreply.github.com>
2020-05-31 21:15:09 -0400

    Update Main.java

--- chess/Main.java
+++ chess/Main.java
@@ -1,5 +1,7 @@
 package chess;
 
+import java.util.List;
+
 /** Main class of the Chess program.
  * @author Wan Fung Chui
  */
@@ -9,10 +11,27 @@ public class Main {
     /** Opens and begins a new game of chess. */
     public static void main(String... dummy) {
         Game game = new Game();
+        ChessPlayer random=new RandomPlayer(game);
         List<Move> validMoves=game.listValidMoves();
-        for(Move m : validMoves) {
-        	System.out.println(m.movedPiece().type().abbrev());
+        int x=0;
+        /*while(true) {
+        	x++;
+        	if(x%10000==0) {
+        	for(Move m : validMoves)
+        		System.out.println(m.movedPiece().type().abbrev());
+        		System.out.println("BREAK BREAK BREAK BREAK");
+        		System.out.println("");
+        		System.out.println("");
+        	}
+        }*/
+        while(true) {
+        	System.out.println(game.turn().abbrev());
+        	if(game.turn().string().contentEquals("BLACK")) {
+	        	Move m=random.getMove();
+	        	game.makeMove(m);
+	        	System.out.println("moved");
+        	}
         }
-    }
 
+    }
 }
