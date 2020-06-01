ddace71c29a38dedfd1927df5c5d7adcd6315499
BilliamBob <65982795+BilliamBob@users.noreply.github.com>
2020-05-31 20:45:08 -0400

    Update Queen.java

--- chess/Queen.java
+++ chess/Queen.java
@@ -193,6 +193,12 @@ public class Queen implements Piece {
             return true;
         }
     }
+    
+     @Override
+    public String getLocation() {
+    	return (_x + ", " + _y);
+    }
+
 
     /** The game this piece belongs to. */
     private Game _game;
